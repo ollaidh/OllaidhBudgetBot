2876359d644242c6597127172e823b755781844d
ollaidh <ollaidh@programmer.net>
2023-11-27 20:27:18 +0200

    Use hardcoded version

--- commands/command_version.py
+++ commands/command_version.py
@@ -3,5 +3,5 @@ from date_utils import get_date_today
 
 class VersionCommandExecutor:
     def execute(self, database_adapter, parameters: list[str]) -> dict:
-        version = get_date_today().replace("-", ".")
+        version = '1.0.0'
         return {'message': f'Bot version: {version}'}
