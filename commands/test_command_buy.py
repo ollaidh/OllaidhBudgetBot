b0ed8eb9a1519270890c835d7f21a2b593ca7a99
ollaidh <ollaidh@programmer.net>
2025-12-06 10:14:12 +0200

    Migrate from unittest to pytest

--- commands/test_command_buy.py
+++ commands/test_command_buy.py
@@ -1,56 +1,51 @@
-import unittest
+import pytest
 from unittest.mock import MagicMock
 from commands.command_buy import *
 from db_adapters.adapter import PurchaseInfo
 
 
-class TestBuy(unittest.TestCase):
-    def test_get_category(self):
-        self.assertEqual(get_category("water"), "utilities")
-        self.assertEqual(get_category("candibober"), "uncategorized")
-
-    def test_validate_buy_parameters(self):
-        buy = BuyCommandExecutor()
-        self.assertEqual(buy.validate(["coffee", "2.5", "takeout"]), PurchaseInfo("coffee", 2.5, "takeout"))
-
-        self.assertRaisesRegex(
-            InvalidNumberParametersException,
-            "Invalid number of parameters!",
-            buy.validate,
-            ["coffee", 0, 12, "takeout"],
-        )
-
-        self.assertRaisesRegex(
-            NegativePriceException, "Invalid price: negative!", buy.validate, ["coffee", -5, "takeout"]
-        )
-
-        self.assertRaisesRegex(
-            NotNumberPriceException, "Invalid price: not a number!", buy.validate, ["coffee", "O.O", "takeout"]
-        )
-
-    def test_command_execute(self):
-        command = BuyCommandExecutor()
-
-        class TestAdapter:
-            pass
-
-        adapter = TestAdapter()
-        adapter.add_purchase = MagicMock(return_value=True)
-        result = command.execute(adapter, ["coffee", 2.5, "takeout"])
-        self.assertTrue(result["message"].startswith("ADDED PURCHASE: coffee 2.5 takeout"))
-        adapter.add_purchase.assert_called_once_with(PurchaseInfo("coffee", 2.5, "takeout"))
-
-        adapter.add_purchase.reset_mock()
-
-        adapter.add_purchase = MagicMock(return_value=False)
-        self.assertRaisesRegex(
-            FailedAccessDatabaseException,
-            "No access to database. Failed to add purchase!",
-            command.execute,
-            adapter,
-            ["chicken", 8, "meat"],
-        )
+@pytest.mark.unit_test
+def test_get_category():
+    assert get_category("water") == "utilities"
+    assert get_category("candibober") == "uncategorized"
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.unit_test
+def test_validate_buy_parameters():
+    buy = BuyCommandExecutor()
+    assert buy.validate(["coffee", "2.5", "takeout"]) == PurchaseInfo("coffee", 2.5, "takeout")
+
+    with pytest.raises(InvalidNumberParametersException) as ex:
+        buy.validate(["coffee", 0, 12, "takeout"])
+        assert str(ex.exception) == "Invalid number of parameters!"
+
+    with pytest.raises(NegativePriceException) as ex:
+        buy.validate(["coffee", -5, "takeout"])
+        assert str(ex.exception) == "Invalid price: negative!"
+
+    with pytest.raises(NotNumberPriceException) as ex:
+        buy.validate(["coffee", "one euro", "takeout"])
+        assert str(ex.exception) == "Invalid price: not a number!"
+
+
+@pytest.mark.unit_test
+def test_command_execute():
+    command = BuyCommandExecutor()
+
+    class TestAdapter:
+        pass
+
+    adapter = TestAdapter()
+    adapter.add_purchase = MagicMock(return_value=True)
+    result = command.execute(adapter, ["coffee", 2.5, "takeout"])
+
+    assert result["message"].startswith("ADDED PURCHASE: coffee 2.5 takeout")
+
+    adapter.add_purchase.assert_called_once_with(PurchaseInfo("coffee", 2.5, "takeout"))
+    adapter.add_purchase.reset_mock()
+
+    adapter.add_purchase = MagicMock(return_value=False)
+
+    with pytest.raises(FailedAccessDatabaseException) as ex:
+        command.execute(adapter, ["chicken", 8, "meat"])
+        assert str(ex.exception) == "No access to database. Failed to add purchase!"
