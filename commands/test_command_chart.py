30f0907c5cca59e61753d0731bd18db58fb13d47
ollaidh <ollaidh@programmer.net>
2025-12-08 19:39:52 +0200

    Migrate from unittest to pytest

--- commands/test_command_chart.py
+++ commands/test_command_chart.py
@@ -1,34 +1,28 @@
-import unittest
+import pytest
 from commands.command_chart import *
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
 
-class TestChart(unittest.TestCase):
-    @patch("commands.command_chart.piechart_maker")
-    def test_command_execute(self, path_mock):
-        command = ChartCommandExecutor()
+@pytest.mark.unit_test
+@patch("commands.command_chart.piechart_maker")
+def test_command_execute(path_mock):
+    command = ChartCommandExecutor()
 
-        class TestAdapter:
-            pass
+    class TestAdapter:
+        pass
 
-        adapter = TestAdapter()
+    adapter = TestAdapter()
 
-        adapter.calculate_spent = MagicMock(return_value={"TOTAL": 100, "meat": 70, "takeaway": 30})
+    adapter.calculate_spent = MagicMock(return_value={"TOTAL": 100, "meat": 70, "takeaway": 30})
 
-        path_mock.return_value = r"C:\Users\razer\PycharmProjects\budget_bot\spent.png"
-        self.assertEqual(
-            {"chart_path": r"C:\Users\razer\PycharmProjects\budget_bot\spent.png"},
-            command.execute(adapter, ["2022-12", "2023-02", "$each"]),
-        )
+    path_mock.return_value = r"C:\Users\razer\PycharmProjects\budget_bot\spent.png"
+    assert command.execute(adapter, ["2022-12", "2023-02", "$each"]) == {
+        "chart_path": r"C:\Users\razer\PycharmProjects\budget_bot\spent.png"
+    }
 
-        adapter.calculate_spent = MagicMock(return_value={"MEAT": 100, "steak": 50, "chicken": 50})
+    adapter.calculate_spent = MagicMock(return_value={"MEAT": 100, "steak": 50, "chicken": 50})
 
-        self.assertEqual(
-            {"chart_path": r"C:\Users\razer\PycharmProjects\budget_bot\spent.png"},
-            command.execute(adapter, ["2022-12", "2023-02", "meat"]),
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert command.execute(adapter, ["2022-12", "2023-02", "meat"]) == {
+        "chart_path": r"C:\Users\razer\PycharmProjects\budget_bot\spent.png"
+    }
